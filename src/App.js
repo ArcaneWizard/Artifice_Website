620ce1ac657c687409870a82bb73b5ced2f180fa
Glacion Ninja <glacionninja@gmail.com>
2020-08-13 23:02:18 -0700

    database stuff added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header /> 
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/posts" component={Post}/>
